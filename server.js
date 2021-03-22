e940e64a2edc58c5c71455bfe68af6cce9b817d5
Adam L <alane019@gmail.com>
2021-03-22 07:25:32 -0400

    add books schema model

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//models
+require("./models/books");
+
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+  //path to routes:
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
