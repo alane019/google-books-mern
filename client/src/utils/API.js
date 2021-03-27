ebbae030fa93226948a4c25f13a16b82b81a0b73
Adam L <alane019@gmail.com>
2021-03-27 16:15:25 -0400

    update api file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,14 +1,10 @@
 import axios from "axios"; 
 
 const baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-// const apiKey = process.env.apikey; 
-// console.log({apiKey});
 
 
 export default {
         getSearchResults: function(query) {
         return axios.get(baseUrl + query)
-          // .then((resp) => {console.log(resp)})
-          // .catch(err => console.log(err))
     }
 };
