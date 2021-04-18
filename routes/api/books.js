b752e0afda158408d58b209078608e5433ccc153
Adam L <alane019@gmail.com>
2021-04-18 16:16:29 -0400

    update to book routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -5,13 +5,17 @@ const booksController = require("../../controllers/booksController");
 
 //API Matches "api/books"
 router.route("/")
-    .get(booksController.findAll)
-    .post(booksController.create); 
+    .post(booksController.create) 
+    .get(booksController.findAll);
+
+//Search by title
+router.route("/search/title")
+    .get(booksController.findByTitle);
+
+
 
 // Matches specific book ID ("/api/books/:id")
-
-router 
-    .route("/:id")
+router.route("book/:id")
     .get(booksController.findById)
     .put(booksController.update)
     .delete(booksController.remove);
